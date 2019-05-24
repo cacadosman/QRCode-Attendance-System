22928ecac5d807caa7b735d13051274267411a0d
cacadosman <wetmanz23@gmail.com>
2019-05-24 11:21:47 +0700

    add students, lecturers, and users migration

--- gateway-service/app/Http/Controllers/ExampleController.php
+++ gateway-service/app/Http/Controllers/ExampleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 class ExampleController extends Controller
 {
     /**
--- gateway-service/app/Providers/AppServiceProvider.php
+++ gateway-service/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
