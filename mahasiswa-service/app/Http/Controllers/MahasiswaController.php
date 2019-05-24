7238816f81f162bd764fc9444a91e24edc94d151
Ichsanul Akbar <ichsanakbar123@gmail.com>
2019-05-24 14:05:52 +0700

    nambah query di controller

--- mahasiswa-service/app/Http/Controllers/MahasiswaController.php
+++ mahasiswa-service/app/Http/Controllers/MahasiswaController.php
@@ -14,16 +14,39 @@ class MahasiswaController extends Controller
         //
     }
     public function notifications(Request $request){
-        $user=$request->user;
+        $user_id=$request->user_id;
+        //$status = DB::table('users AS u')
+                      //->join('students AS s','u.id','=','s.user_id')
+        $status = DB::table('students AS s')
+                      ->join('student_attendances AS s_a','s.id','=','s_a.student_id')
+                      ->join('class_attendances AS c_a','c_a.id ','=','s_a.class_attendances_id')
+                      ->join('class_schedules AS c_s','c_s.id','=','c_a.class_schedule_id')
+                      ->join('classes AS c','c.id','=','c_s.class_id')
+                      ->select('s.id','c.class_name','s_a.created_at')
+                      ->where('s.user_id',$user_id)->get();
+
+        return response()->json(['status' => $status],200);
+
     }
     public function presences(Request $request){
-        $user=$request->user;
-        $nim=$request->nim;
-        $qrdata=$request->qrdata;
+        $user_id=$request->user_id;
+        $class_attendance_id=$request->class_attendance_id;
         $geolocation=$request->geolocation;
+        $created_at=time();
+        $status= DB::table('student_attendances AS s_a')->insert(
+            ['class_attendance_id'=>$class_attendance_id,'student_id'=>$user_id,'geolocation'=>$geolocation,'created_at'=>$created_at]
+        );
+        if($status) return response()->json(['status'=>$status],200);
+        else return response()->json(['status'=>$status],403);
     }
     public function courses(Request $request){
-        $user=$request->user;
+        $user_id=$request->user_id;
+
+        $status=DB::table('students AS s')
+                    ->join('student_classes AS s_c','s_c.id','=','s.user_id')
+                    ->join('classes AS c','c.id','=','s_c.class_id')
+                    ->join('courses AS co','co.id','=','c.course_id')
+                    ->select('s.id','group_concat(co.course_name)')->where('s.user_id',$user_id);
     }
     //
 }
