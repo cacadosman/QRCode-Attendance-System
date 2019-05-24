853e1a0575105ec8aaded60a2cad7b8fea8da053
Ichsanul Akbar <ichsanakbar123@gmail.com>
2019-05-24 11:43:38 +0700

    bikin table

---
+++ gateway-service/database/migrations/2019_05_24_063255_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('study_program_id');
+            $table->string('code');
+            $table->string('type');
+            $table->string('semester');
+            $table->int('credit');
+            $table->string('status');
+          //  $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
