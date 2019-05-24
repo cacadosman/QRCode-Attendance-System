c19b88e59eb5af04bc36f0d916b3a4ee3ccd8fab
Blood-Hound <ichsanakbar123@gmail.com>
2019-05-24 11:07:12 +0700

    bikin controller

--- mahasiswa-service/routes/web.php
+++ mahasiswa-service/routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/courses',"MahasiswaController@courses");
+$router->post('/notifications',"MahasiswaController@notifications");
+$router->post('/presences',"MahasiswaController@presences");
