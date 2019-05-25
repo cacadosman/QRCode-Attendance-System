2bda534a7aeaba7f6a631cc25342e80ccc53af44
cacadosman <wetmanz23@gmail.com>
2019-05-25 20:38:34 +0700

    edit GeneratorController untuk meng-generate QRCode

--- gateway-service/app/Http/Controllers/GeneratorController.php
+++ gateway-service/app/Http/Controllers/GeneratorController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class GeneratorController
 {
     public function generate(Request $request)
     {
+        $client = new Client([
+            "base_uri" => env('APP_URL') . env('GENERATOR_LOCAL')
+        ]);
 
+        $response = $client->request('POST', '/generate', [
+            'json' => [
+                'class_schedule_id' => $request->class_schedule_id,
+                'time' => $request->time
+            ]
+        ]);
+
+        return json_decode($response->getBody(), true);
     }
 }
