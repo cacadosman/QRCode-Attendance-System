ad865210eadab8cd66ec0bb57b331c116b5d1b18
cacadosman <wetmanz23@gmail.com>
2019-05-25 20:38:11 +0700

    edit StudentsMiddleware untuk validasi nilai kembalian

--- gateway-service/app/Http/Middleware/StudentsMiddleware.php
+++ gateway-service/app/Http/Middleware/StudentsMiddleware.php
@@ -5,12 +5,16 @@ namespace App\Http\Middleware;
 
 use App\Http\Services\JwtService;
 use Closure;
+use Illuminate\Http\JsonResponse;
 
 class StudentsMiddleware
 {
     public function handle($request, Closure $next, $guard = null)
     {
         $auth = JwtService::handle($request->token);
+
+        if ($auth instanceof JsonResponse)
+            return $auth;
 
         if ($auth->role !== "mahasiswa")
         {
