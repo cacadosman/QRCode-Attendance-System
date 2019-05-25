84630a4da642165443b103e0c848535b83b376bc
cacadosman <wetmanz23@gmail.com>
2019-05-25 20:21:55 +0700

    add routes

--- generator-service/routes/web.php
+++ generator-service/routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/generate', 'QRController@generate');
+$router->post('/decode', 'QRController@decode');
